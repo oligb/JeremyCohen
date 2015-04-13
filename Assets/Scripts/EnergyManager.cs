f663596b02cdbf7b79994fbf045d2be795a50267
oliver garcia-borg <oligb7@gmail.com>
2015-04-13 13:40:41 -0400

    Prototyped movement
    Prototyped input, player structure

---
+++ Assets/Scripts/EnergyManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyManager : MonoBehaviour {
+
+	public GameObject player;
+	public float energy;
+	// Use this for initialization
+	void Start () {
+		player=GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		energy=player.GetComponent<PlayerMoveQueueing>().currentEnergy;
+		transform.localScale= new Vector3(energy/10f,1f,1f);
+	
+	}
+}
---
+++ Assets/Scripts/PlayerAttacks.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttacks : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject bullet1;
+	public Vector3 targetPos;
+	public Vector3 direction;
+
+	public int numShots=20;
+	public float bulletSpeed=20f;
+	public float shotArc=30f;
+	public Camera mainCam;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Melee(){
+	}
+	public void Shoot(Vector3 playerPos, Vector3 targetPos){
+		Camera.main.gameObject.GetComponent<CamShake>().TriggerShake();
+		direction= targetPos-playerPos;
+		for(int i=0; i<numShots; i++){
+			GameObject bullet= Instantiate(bullet1, playerPos,Quaternion.identity) as GameObject;
+			bullet.transform.LookAt(targetPos);
+			bullet.transform.Rotate(0f,Random.Range(-shotArc/2,shotArc/2),0f);
+			bullet.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward*bulletSpeed*Random.Range(.8f,1.2f),ForceMode.Impulse);
+			
+		}
+	}
+
+}
