c0f85a5394e39ce884a8b80a89aa6f84b8bbb0f8
oliver garcia-borg <oligb7@gmail.com>
2015-04-15 12:44:16 -0400

    Added more architecture, preparation room

--- Assets/Scripts/DestroyAfterTime.cs
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -6,12 +6,18 @@ public class DestroyAfterTime : MonoBehaviour {
 	// Use this for initialization
 	public float startTime=0f;
 	public float lifeTime=5f;
+	public Vector3 startPos, endPos;
+	public float range;
 	void Start () {
 		startTime=Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if(Vector3.Distance(transform.position,startPos)>range){
+			Destroy(gameObject);
+		}
 	
 		if(Time.time-startTime>lifeTime){
 			Destroy(gameObject);
