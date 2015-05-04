7d87b41f4bd5e6e54020437da9d855f561851463
Batu <ba921@nyu.edu>
2015-05-04 15:03:06 -0400

    Exit prefab is here

---
+++ Assets/Scripts/World and logic/Exit.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Exit ran.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log("Something entered");
+		if(col.gameObject.tag == "Player"){
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
