9d7876eddbd47d8d5eec35346bc083933f7691ac
Batu <ba921@nyu.edu>
2015-05-10 19:51:05 -0400

    Difficulty increase started!

--- Assets/Scripts/World/Exit.cs
+++ Assets/Scripts/World/Exit.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class Exit : MonoBehaviour {
 
-	public GameObject mapMaker;
+	public GameObject mapMaker, levelManager;
 	// Use this for initialization
 	void Start () {
 		mapMaker = GameObject.Find("MapMakerV7(Clone)");
-
+		levelManager = GameObject.Find("LevelManager");
 	}
 	
 	// Update is called once per frame
@@ -16,7 +16,12 @@ public class Exit : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider col){
+
+
 		if(col.gameObject.tag == "Player"){
+			levelManager.GetComponent<LevelManager>().level++;
+			levelManager.GetComponent<LevelManager>().IncreaseDifficulty();
+
 			col.gameObject.GetComponent<PlayerStateControls>().MoveToPrepRoom();
 			mapMaker.GetComponent<MapMakerV7>().DestroyTheWorld();
 
