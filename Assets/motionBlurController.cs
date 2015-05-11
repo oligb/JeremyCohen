0a11ee160fb04aa74b6ad74f41e2ee161401f046
oliver garcia-borg <oligb7@gmail.com>
2015-05-11 14:07:37 -0400

    motionblur

---
+++ Assets/motionBlurController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+public class motionBlurController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+			if(!GetComponent<PlayerMoveQueueing>().timeStopped){
+				Camera.main.GetComponent<CameraMotionBlur>().enabled=false;
+			}			
+			else{
+				Camera.main.GetComponent<CameraMotionBlur>().enabled=true;
+			}
+	}
+}
